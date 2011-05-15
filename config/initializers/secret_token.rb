bc1cb4d7133c80534f4fa9503c40f9a9561d3cc3
Anderson Bordin <andersonbordin@gmail.com>
2011-05-14 21:05:22 -0300

    Commit Inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gado::Application.config.secret_token = '981842a19befc45f2096292575835de1d7a435c75bd1fd9dcd51e189a94f7bc53bd7916a562d59df619b353b1e589af4d708fb9d0fc716312c1044d7ed641dd8'
